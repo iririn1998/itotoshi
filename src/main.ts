d6db70e74446de931e541dbc7fde17a9bfb06827
Shungo-I <iririn199810710@gmail.com>
2026-03-22 08:33:28 +0900

    feat: draw basic moving line

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,37 @@
 import "./style.css";
-import { Engine, DisplayMode, Color } from "excalibur";
+import {
+  Engine,
+  DisplayMode,
+  Color,
+  Actor,
+  Vector,
+  vec,
+  type ExcaliburGraphicsContext,
+} from "excalibur";
+
+class LineActor extends Actor {
+  points: Vector[] = [];
+  headPos: Vector = vec(0, 300);
+  velocity: Vector = vec(150, 0);
+
+  onInitialize() {
+    this.points.push(this.headPos.clone());
+  }
+
+  onPreUpdate(_engine: Engine, delta: number) {
+    const dt = delta / 1000;
+
+    this.headPos.x += this.velocity.x * dt;
+    this.points.push(this.headPos.clone());
+  }
+
+  onPostDraw(ctx: ExcaliburGraphicsContext) {
+    if (this.points.length < 2) return;
+    for (let i = 0; i < this.points.length - 1; i++) {
+      ctx.drawLine(this.points[i], this.points[i + 1], Color.White, 3);
+    }
+  }
+}
 
 const game = new Engine({
   canvasElementId: "game",
@@ -8,5 +40,8 @@ const game = new Engine({
   displayMode: DisplayMode.FitScreen,
   backgroundColor: Color.DarkGray,
 });
+
+const lineActor = new LineActor();
+game.add(lineActor);
 
 game.start();
